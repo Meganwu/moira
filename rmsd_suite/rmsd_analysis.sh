2962b28897624b54d604a0997048f6f65a190de9
MeganWu <wunianwhu@gmail.com>
2023-06-06 10:39:55 +0300

    Update rmsd_analysis.sh

--- rmsd_suite/rmsd_analysis.sh
+++ rmsd_suite/rmsd_analysis.sh
@@ -1,19 +1,18 @@
 #! /bin/bash
-filename=`cat  fine.txt`
-cd    /data/mdml/Data/2021-9-10-amber 
+filename=`cat  /data/mdml/neutral_collection/md_222.txt`
+cd   /data/mdml/neutral_collection/neutral_2a
 for  i in $filename
 do
-  cd ${i}MD_5a
-
-  cp  ../total.cpptraj     .  
-
+  cd ${i}MD_neu_2a
+  cp   /data/mdml/neutral_collection/result_sh_all/total_interaction_input/total.cpptraj   .
   iscl=`grep Cl-  complex_vdw_bonded.pdb`
   isna=`grep Na+  complex_vdw_bonded.pdb`
+
   if [ -n "$iscl" ]; then
      noprostart=`grep "Cl-"  complex_vdw_bonded.pdb | head -n 1 | awk '{print $5}'`
-     let proend=$noprostart-1    
+     let proend=$noprostart-1
   elif  [ -n "$isna" ]; then
-     noprostart=`grep "Na+"  complex_vdw_bonded.pdb | head -n 1 | awk '{print $5}'`
+     noprostart=`grep Na+  complex_vdw_bonded.pdb  | head -1  |  awk '{print $5}'`
      let proend=$noprostart-1
   else
      noprostart=`grep "WAT"  complex_vdw_bonded.pdb | head -n 1 | awk '{print $5}'`
@@ -25,6 +24,7 @@ do
   sed -i 's/:3com/:1-'"$proend"'/g'   total.cpptraj
   sed -i 's/:2pro/:2-'"$proend"'/g'   total.cpptraj
   sed -i 's/:4lig/:1/g'   total.cpptraj
-  cd   /data/mdml/Data/2021-9-10-amber  
-done 
 
+  cpptraj total.cpptraj
+  cd   /data/mdml/neutral_collection/neutral_2a
+done
