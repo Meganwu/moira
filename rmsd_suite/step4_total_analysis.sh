60daf1d584943b96ccf4b3cc91cefb58e2438818
MeganWu <wunianwhu@gmail.com>
2023-06-06 10:29:52 +0300

    Add files via upload

---
+++ rmsd_suite/step4_total_analysis.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+filename=`cat  fine.txt`
+cd    /data/mdml/Data/2021-9-10-amber 
+for  i in $filename
+do
+  cd ${i}MD_5a
+
+  cp  ../total.cpptraj     .  
+
+  iscl=`grep Cl-  complex_vdw_bonded.pdb`
+  if [ -n "$iscl" ]; then
+     noprostart=`grep "Cl-"  complex_vdw_bonded.pdb | head -n 1 | awk '{print $5}'`
+     let proend=$noprostart-1
+  else
+     noprostart=`grep "WAT"  complex_vdw_bonded.pdb | head -n 1 | awk '{print $5}'`
+     let proend=$noprostart-1
+  fi
+
+  proend=$(echo $proend)
+  sed -i 's/:1/:1-'"$proend"'/g'   total.cpptraj
+  sed -i 's/:3com/:1-'"$proend"'/g'   total.cpptraj
+  sed -i 's/:2pro/:2-'"$proend"'/g'   total.cpptraj
+  sed -i 's/:4lig/:1/g'   total.cpptraj
+  cd   /data/mdml/Data/2021-9-10-amber  
+done 
+
