d2c9ed9176cd1c14eb7186196c875e8891398e9a
MeganWu <wunianwhu@gmail.com>
2023-06-06 10:12:59 +0300

    Update step3_run_md.sh

--- md_suite/step3_run_md.sh
+++ md_suite/step3_run_md.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+pdblist=`cat  md_222.txt` 
+
+for id in $pdblist
+do
+     j=`echo ${id:0:4}`
+     md_name2=${j}MD2_neu
+     cd   native_neutral/$md_name2
+     cp  /data/wunian2/neutral_collection/inputs/*     .
+     ./pmemd.sh
+     cd   /data/wunian2/neutral_collection
+done
